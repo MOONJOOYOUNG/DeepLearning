965049043960b9b7c2057bb6fcf0daf15f6114f4
문주영 <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-07-03 19:50:20 +0900

    Add files via upload

---
+++ Batch Normalization/Batch Normalization_MNIST.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import random
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(777)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])
+Y = tf.placeholder(tf.float32, [None, 10])
+phase = tf.placeholder(tf.bool, name='phase')
+
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 8], stddev=0.01))
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.layers.batch_normalization(L1,center=True, scale=True,
+                                   training=phase)
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+
+W2 = tf.Variable(tf.random_normal([3, 3, 8, 16], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.layers.batch_normalization(L2,center=True, scale=True,
+                                   training=phase)
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L2_flat = tf.reshape(L2, [-1, 7 * 7 * 16])
+
+W3 = tf.get_variable("W3", shape=[7 * 7 * 16, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b = tf.Variable(tf.random_normal([10]))
+logits = tf.matmul(L2_flat, W3) + b
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+print('Learning started. It takes sometime.')
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # 테스트 시 phase 0 지정.
+        feed_dict = {X: batch_xs, Y: batch_ys, 'phase:0': 1}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+print('Learning Finished!')
+
+feed_dict = {X: batch_xs, Y: batch_ys, 'phase:0': 0}
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels}))
+
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(logits, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
